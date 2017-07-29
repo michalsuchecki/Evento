a87a4060201b578602cb87d3dcd84e4b3024243a
Michał Suchecki <michal_suchecki@outlook.com>
2017-07-29 12:07:34 +0200

    addded base domain classes

---
+++ src/Evento.Core/Domain/Ticket.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Evento.Core.Domain
+{
+    public class Ticket : Entity
+    {
+        public Guid EventId { get; protected set; }
+        public int Seating {get; protected set; }
+        public decimal Price {get; protected set; }
+        public Guid? UserId {get; protected set; }
+        public string UserName {get; protected set; }
+        public DateTime? PurchaseAt {get; protected set; }
+        public bool Purchased => PurchaseAt.HasValue;
+
+        protected Ticket()
+        {
+
+        }
+
+        public Ticket(Event @event, int seating, decimal price)
+        {
+            EventId = @event.Id;
+            Seating = seating;
+            Price = price;
+        }
+
+    }
+}
